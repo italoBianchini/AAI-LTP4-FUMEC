111fd144215d6149820d5ada6f8a3a65631af6ab
Ítalo <talo@not>
2016-05-04 01:15:02 -0300

    Criando ClienteDAO

---
+++ src/dao/ClienteDAO.java
@@ -0,0 +1,57 @@
+package dao;
+
+import Exception.PersistenciaException;
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import model.ClienteModel;
+
+/**
+ * A classe <b>ClienteDAO</b> implementa os metodos de acesso ao banco
+ * relacionados ao objeto <b>ClienteModel</b>.<br>
+ * Pacote dao.
+ *
+ * @author Ítalo Bianchini
+ * @since Maio, 2016
+ * @version 1.0
+ */
+public class ClienteDAO implements GenericDAO<ClienteModel> {
+
+    @Override
+    public boolean Inserir(ClienteModel clienteModel) throws PersistenciaException {
+
+        boolean inseriuCorretamente = false;
+
+        Connection connection;
+
+        try {
+
+            connection = Conexao.getInstance().getConnection();
+
+            String sql = "INSERT INTO Clientes (codCliente, Nome, Endereco, Bairro, Cidade, Uf, Cep, Telefone, E_mail, dataCadCliente)" + "VALUES(?,?,?,?,?,?)";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, clienteModel.getCodigoCliente());
+            statement.setString(2, clienteModel.getNome());
+            statement.setString(3, clienteModel.getEndereco());
+            statement.setString(4, clienteModel.getBairro());
+            statement.setString(5, clienteModel.getCidade());
+            statement.setString(6, clienteModel.getUf());
+            statement.setString(7, clienteModel.getCep());
+            statement.setString(8, clienteModel.getTelefone());
+            statement.setString(9, clienteModel.getEmail());
+            statement.setDate(10, clienteModel.getDataDeCadastro());
+
+            statement.execute();
+            connection.close();
+
+            inseriuCorretamente = true;
+
+        } catch (Exception e) {
+            throw new PersistenciaException(e.getMessage(), e);
+        }
+        return inseriuCorretamente;
+    }
+
+}
