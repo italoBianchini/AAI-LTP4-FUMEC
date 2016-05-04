92bbfc2da5b598da8e04f1db19452172431650f1
Ítalo <talo@not>
2016-05-04 01:05:44 -0300

    Excluindo classe incorreta

--- src/dao/GenericDAO.java
+++ src/dao/GenericDAO.java
@@ -1,9 +1,29 @@
 package dao;
 
+import Exception.PersistenciaException;
+
 /**
+ * A classe <b>GenericDAO</b> implementa metodos genericos data access. Pacote
+ * dao.
  *
- * @author Ítalo
+ * @author Ítalo Bianchini
+ * @param <genericObject>
+ * @since Maio, 2016
+ * @version 1.0
  */
-public class GenericDAO {
-    
+public class GenericDAO<genericObject> {
+
+    /**
+     * Método generico para inserir dados no banco
+     *
+     * @param obj
+     * @return booleano
+     * @throws PersistenciaException
+     * @since Maio, 2016
+     */
+    public interface GenericDAO<genericObject> {
+
+        boolean Inserir(genericObject obj) throws PersistenciaException;
+    }
+
 }
